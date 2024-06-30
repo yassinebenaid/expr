2d0bce9d2871a043dc269874d1bfb8e08b10fe7f
yassinebenaid <yassinebenaide3@gmail.com>
2024-06-30 18:45:02 +0100

    remove ast.go, and move its content to parser.go

--- parser.go
+++ parser.go
@@ -5,6 +5,35 @@ import (
 	"strconv"
 )
 
+type expression interface {
+	ToString() string
+}
+
+type infix struct {
+	left     expression
+	operator token
+	right    expression
+}
+
+func (i infix) ToString() string {
+	return fmt.Sprintf("(%v %v %v)", i.left.ToString(), i.operator.literal, i.right.ToString())
+}
+
+type prefix struct {
+	operator token
+	operand  expression
+}
+
+func (i prefix) ToString() string {
+	return fmt.Sprintf("(%v%v)", i.operator.literal, i.operand.ToString())
+}
+
+type numberLiteral float64
+
+func (i numberLiteral) ToString() string {
+	return fmt.Sprint(i)
+}
+
 type parser struct {
 	l         *lexer
 	currToken token
