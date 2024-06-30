3cba2cbd724382eb645f6a2ae9e91489e1ec181e
yassinebenaid <yassinebenaide3@gmail.com>
2024-06-30 18:41:22 +0100

    added infix eval support

--- eval.go
+++ eval.go
@@ -14,6 +14,8 @@ func eval(exp expression) Number {
 		return Number(v)
 	case prefix:
 		return evalPrefix(v)
+	case infix:
+		return evalInfix(v)
 	default:
 		panic("eval: expression not supported")
 	}
@@ -29,5 +31,30 @@ func evalPrefix(exp prefix) Number {
 		return value
 	default:
 		panic("evalPrefix: prefix is undefined")
+	}
+}
+
+func evalInfix(exp infix) Number {
+	l, r := eval(exp.left), eval(exp.right)
+
+	switch exp.operator._type {
+	case _T_SUB:
+		return l - r
+	case _T_ADD:
+		return l + r
+	case _T_MUL:
+		return l * r
+	case _T_DEV:
+		return l / r
+	case _T_BINOR:
+		return Number(int64(l) | int64(r))
+	case _T_BINAND:
+		return Number(int64(l) & int64(r))
+	case _T_BINLSHIFT:
+		return Number(int64(l) << int64(r))
+	case _T_BINRSHIFT:
+		return Number(int64(l) >> int64(r))
+	default:
+		panic("evalPrefix: operator is undefined")
 	}
 }
